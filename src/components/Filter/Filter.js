b2472d120faa284106e4ecf480b5b7814b7a1754
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-09-24 23:33:48 +0300

    add total cout

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { StyledFilter, StyledInput, StyledTitle } from './Filter.styled';
 import { filterByName } from 'redux/filterSlice';
-import { selectFilter } from 'redux/selectors';
+import { selectCount, selectFilter } from 'redux/selectors';
 
 export const Filter = () => {
   const filter = useSelector(selectFilter);
@@ -11,11 +11,13 @@ export const Filter = () => {
     const value = evt.target.value;
     dispatch(filterByName(value));
   };
-
+  const count = useSelector(selectCount);
   return (
     <StyledFilter>
       <StyledTitle>Find contacts by name</StyledTitle>
+
       <StyledInput type="text" value={filter} onChange={handleFilterName} />
+      <StyledTitle>Total contacts: {count} </StyledTitle>
     </StyledFilter>
   );
 };
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,11 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts;
 export const selectFilter = state => state.filter;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectItems = state => state.contacts.items;
+
+export const selectCount = createSelector([selectItems], items => {
+  return items.length;
+});
